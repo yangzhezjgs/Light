cbea51f363ea3c427acbb3ff6dcff552e0663db8
yzzjgs <764127031@qq.com>
2017-08-01 07:21:04 -0700

    add errorhandle

--- __init__.py
+++ __init__.py
@@ -2,6 +2,21 @@
 from werkzeug.wrappers import Response,Request
 from Light.Route import Route
 
+ERROR_MAP = {
+    '401': Response('<h1>401 Unknown or unsupported method</h1>', content_type='text/html; charset=UTF-8', status=401),
+    '404': Response('<h1>404 Source Not Found<h1>', content_type='text/html; charset=UTF-8', status=404),
+    '503': Response('<h1>503 Unknown function type</h1>', content_type='text/html; charset=UTF-8',  status=503)
+}
+
+# 定义文件类型
+TYPE_MAP = {
+    'css':  'text/css',
+    'js': 'text/js',
+    'png': 'image/png',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg'
+}
+
 class Light:
 
 	def __init__(self):
