96d71427f3026fefa5614b6538ff57d4450335ab
yzzjgs <764127031@qq.com>
2017-08-12 20:59:00 +0800

    add support mvc

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 #coding:utf-8
 from werkzeug.wrappers import Response,Request
 from Light.Route import Route
-from Light.exceptions as exceptions
-from Light.helper import parse_stat
+import Light.exceptions as exceptions
+from Light.helper import parse_static_key
+from werkzeug.serving import run_simple
 ERROR_MAP = {
     '401': Response('<h1>401 Unknown or unsupported method</h1>', content_type='text/html; charset=UTF-8', status=401),
     '404': Response('<h1>404 Source Not Found<h1>', content_type='text/html; charset=UTF-8', status=404),
@@ -37,15 +38,47 @@ class Light:
 
 	def __call__(self,environ,start_response):
 		return self.wsgi_app(environ,start_response)
-	
+	def dispatch_static(self, static_path):
+		if os.path.exists(static_path):
+			key = parse_static_key(static_path)
+			doc_type = TYPE_MAP.get(key, 'text/plain')
+			with open(static_path, 'rb') as f:
+				rep = f.read()
+			return Response(rep, content_type=doc_type)
+		else:
+			return ERROR_MAP['404']
+
 	def dispatch_request(self,request):
 		url = "/" + "/".join(request.url.split("/")[3:]).split("?")[0]
-		
-		rep = self.url_map[url]()
-		status = 200  
-		headers = {
-			 'Server': 'Shiyanlou Framework'
-		 }
+		if url.find(self.static_folder) == 1 and url.index(self.static_folder) == 1:
+			endpoint = 'static'
+			url = url[1:]
+		else:
+			endpoint = self.url_map.get(url,None)
+		headers = {'Server':'Light Web 0.1'}
+		if endpoint is None:
+			return ERROR_MAP['404']
+		exec_function = self.function_map[endpoint]
+		if exec_function.func_type == 'route':
+			if request.method in exec_function.options.get('methods'):
+				argcount = exec_function.func.__code__.co_argcount
+				if argcount > 0:
+					rep = exec_function.func(request)
+				else:
+					rep = exec_function.func()
+			else:
+				return ERROR_MAP['401']
+		elif exec_function.func_type == 'view':
+			rep = exec_function.func(request)
+		elif exec_function.func_type == 'static':
+			return exec_function.func(url)
+		else:
+			return ERROR_MAP['503']
+		status = 200
+		content_type = 'text/html'
+		return Response(rep,content_type='%s;charset=UTF-8'%content_type,headers = headers,status = status)
+
+
 
 		return Response(rep, content_type='text/html', headers=headers, status=status)
 		
@@ -53,7 +86,9 @@ class Light:
 		request = Request(environ)
 		response = self.dispatch_request(request)
 		return response(environ, start_response)
-
+	
+	def bind_view(self,url,view_class,endpoint):
+		self.add_url_rule(url,func=view_class.get_func(endpoint),func_type='view')
 	def add_url_rule(self,url,func,func_type,endpoint=None,**options):
 		if endpoint is None:
 			endpoint = func.__name__
@@ -63,13 +98,17 @@ class Light:
 			raise exceptions.EndpointExistsError
 		self.url_map[url] = endpoint
 		self.function_map[endpoint] = ExecFunc(func,func_type,**options)
-
+	def load_controller(self,controller):
+		name = controller.__name__()
+		for rule in controller.url_map:
+			self.bind_view(rule['url'],rule['view'],name + '.'+rule['endpoint'])
 	def run(self,host=None,port=None,**options):
-		for key,value in options.item():
+		for key,value in options.items():
 			if value is not None:
 				self.__setattr__(key,value)
 			if host:
 				self.host = host
 			if port:
 				self.port = port
+		self.function_map['static'] = ExecFunc(func=self.dispatch_static,func_type='static')
 		run_simple(hostname=self.host,port=self.port,application=self,**options)
