8df5c01e6192f6ad2099997d746abfa72b96643a
yzzjgs <764127031@qq.com>
2017-08-12 19:26:22 +0800

    add exceptions and tool

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 #coding:utf-8
 from werkzeug.wrappers import Response,Request
 from Light.Route import Route
-
+from Light.exceptions as exceptions
+from Light.helper import parse_stat
 ERROR_MAP = {
     '401': Response('<h1>401 Unknown or unsupported method</h1>', content_type='text/html; charset=UTF-8', status=401),
     '404': Response('<h1>404 Source Not Found<h1>', content_type='text/html; charset=UTF-8', status=404),
@@ -17,10 +18,21 @@ TYPE_MAP = {
     'jpeg': 'image/jpeg'
 }
 
+class ExecFunc:
+	def __init__(self,func,func_type,**options):
+		self.func = func
+		self.options = options
+		self.func_type = func_type
+
 class Light:
 
-	def __init__(self):
+	def __init__(self,static_folder='static'):
+		self.host = '127.0.0.1'
+		self.port = 8080
 		self.url_map = {}
+		self.static_map = {}
+		self.function_map = {}
+		self.static_folder = static_folder
 		self.route = Route(self)
 
 	def __call__(self,environ,start_response):
@@ -42,7 +54,22 @@ class Light:
 		response = self.dispatch_request(request)
 		return response(environ, start_response)
 
-	def add_url_rule(self,url,func,**options):
-		self.url_map[url] = func
+	def add_url_rule(self,url,func,func_type,endpoint=None,**options):
+		if endpoint is None:
+			endpoint = func.__name__
+		if url in self.url_map:
+			raise exceptions.URLExistsError
+		if endpoint in self.function_map and func_type != 'static':
+			raise exceptions.EndpointExistsError
+		self.url_map[url] = endpoint
+		self.function_map[endpoint] = ExecFunc(func,func_type,**options)
 
-
+	def run(self,host=None,port=None,**options):
+		for key,value in options.item():
+			if value is not None:
+				self.__setattr__(key,value)
+			if host:
+				self.host = host
+			if port:
+				self.port = port
+		run_simple(hostname=self.host,port=self.port,application=self,**options)
